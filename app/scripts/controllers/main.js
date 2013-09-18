6d4c6b3376c31113d0d64b0fca0536bc0374e8e4
Christoph Lipautz <christoph@lipautz.org>
2013-09-18 23:22:33 +0200

    minor style changes

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,9 +4,9 @@ angular.module('yeomanTaskboardApp')
   .controller('MainCtrl', function ($scope) {
     // $scope.tasks = {
     $scope.impediments = {
-      impurgent: ["something very important and urgent", "something also very important", "this has to be done very first", "we need more chicken"],
-      urgent: ["something to do tomorrow"],
-      important: ["something real important", "do you need some callsigns!!!!!!"],
-      none: ["something to do later", "UPPERCASE IMPLIES IT IS IMPORTANT"]
+      impurgent: ["something very important and urgent", "something also very important", "store tasks", "this has to be done very first", "we need more chicken"],
+      urgent: ["something to do tomorrow", "set same height for all sections", "move tasks between sections"],
+      important: ["we need a done action", "something real important", "do you need some callsigns!!!!!!", "add add action, hehe add add ;)"],
+      none: ["something to do later", "UPPERCASE IMPLIES IT IS IMPORTANT", "add delete action"]
     };
   });
