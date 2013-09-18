c2a3b65d92202f28305cec96d7ff4199f39dc1b7
Christoph Lipautz <christoph@lipautz.org>
2013-09-18 21:48:39 +0200

    adding some demo tasks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,9 +3,10 @@
 angular.module('yeomanTaskboardApp')
   .controller('MainCtrl', function ($scope) {
     // $scope.tasks = {
-    $scope.impurgents = ["something very important and urgent", "something also very important"];
-    //   urgent: ["something to do tomorrow"],
-    //   important: ["something real important"],
-    //   none: ["something to do later"]
-    // };
+    $scope.impediments = {
+      impurgent: ["something very important and urgent", "something also very important", "this has to be done very first", "we need more chicken"],
+      urgent: ["something to do tomorrow"],
+      important: ["something real important", "do you need some callsigns!!!!!!"],
+      none: ["something to do later", "UPPERCASE IMPLIES IT IS IMPORTANT"]
+    };
   });
