0b7689f9be22500a107c27bb806c04bee4ff9029
Christoph Lipautz <christoph@lipautz.org>
2013-09-18 20:45:35 +0200

    adding basic structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('yeomanTaskboardApp')
+  .controller('MainCtrl', function ($scope) {
+    // $scope.tasks = {
+    $scope.impurgents = ["something very important and urgent", "something also very important"];
+    //   urgent: ["something to do tomorrow"],
+    //   important: ["something real important"],
+    //   none: ["something to do later"]
+    // };
+  });
